3b229fced88ca4fb2de91c5343ff3180e8b9663c
elias teeny <elias.teeny@gmail.com>
2020-11-22 17:59:20 +0200

    refactor document scanner widget

--- lib/document_scanner.dart
+++ lib/document_scanner.dart
@@ -7,44 +7,49 @@ import 'package:flutter/services.dart';
 
 export 'package:document_scanner/scannedImage.dart';
 
-class DocumentScanner extends StatefulWidget {
-  final Function(String) onPictureTaken;
-  final MethodChannel channel = const MethodChannel('document_scanner');
+const String _methodChannelIdentifier = 'document_scanner';
 
-  final bool documentAnimation;
-  final String overlayColor;
-  final int detectionCountBeforeCapture;
-  final int detectionRefreshRateInMS;
-  final bool enableTorch;
-  final bool useFrontCam;
-  final double brightness;
-  final double saturation;
-  final double contrast;
-  final double quality;
-  final bool useBase64;
-  final bool saveInAppDocument;
-  final bool captureMultiple;
-  final bool manualOnly;
-  final bool noGrayScale;
+/// onDocumentScanned gets called when the scanner successfully scans a rectangle (document)
+
+class DocumentScanner extends StatefulWidget {
+  final Function(ScannedImage) onDocumentScanned;
+
+  // final bool documentAnimation;
+  // final String overlayColor;
+  // final int detectionCountBeforeCapture;
+  // final int detectionRefreshRateInMS;
+  // final bool enableTorch;
+  // final bool useFrontCam;
+  // final double brightness;
+  // final double saturation;
+  // final double contrast;
+  // final double quality;
+  // final bool useBase64;
+  // final bool saveInAppDocument;
+  // final bool captureMultiple;
+  // final bool manualOnly;
+  // final bool noGrayScale;
 
   DocumentScanner({
-    @required this.onPictureTaken,
-    this.documentAnimation,
-    this.overlayColor,
-    this.detectionCountBeforeCapture,
-    this.detectionRefreshRateInMS,
-    this.enableTorch,
-    this.useFrontCam,
-    this.brightness,
-    this.saturation,
-    this.contrast,
-    this.quality,
-    this.useBase64,
-    this.saveInAppDocument,
-    this.captureMultiple,
-    this.manualOnly,
-    this.noGrayScale,
+    @required this.onDocumentScanned,
+    // this.documentAnimation,
+    // this.overlayColor,
+    // this.detectionCountBeforeCapture,
+    // this.detectionRefreshRateInMS,
+    // this.enableTorch,
+    // this.useFrontCam,
+    // this.brightness,
+    // this.saturation,
+    // this.contrast,
+    // this.quality,
+    // this.useBase64,
+    // this.saveInAppDocument,
+    // this.captureMultiple,
+    // this.manualOnly,
+    // this.noGrayScale,
   });
+
+  final MethodChannel _channel = const MethodChannel(_methodChannelIdentifier);
 
   @override
   _DocState createState() => _DocState();
@@ -53,36 +58,35 @@ class DocumentScanner extends StatefulWidget {
 class _DocState extends State<DocumentScanner> {
   @override
   void initState() {
-    print("initializing state");
-    widget.channel.setMethodCallHandler((MethodCall call) {
-      print("on method call handler");
-      if (call.method == "onPictureTaken") {
-        print("on document scanned with args : " + call.arguments.toString());
-        Map<String, dynamic> argsAsMap =
-            Map<String, dynamic>.from(call.arguments);
-
-        widget.onPictureTaken(argsAsMap["croppedImage"]);
-      }
-    });
+    print("initializing document scanner state");
+    widget._channel.setMethodCallHandler(_onDocumentScanned);
     super.initState();
+  }
+
+  Future<dynamic> _onDocumentScanned(MethodCall call) async {
+    if (call.method == "onPictureTaken") {
+      Map<String, dynamic> argsAsMap =
+          Map<String, dynamic>.from(call.arguments);
+
+      widget.onDocumentScanned(ScannedImage.fromMap(argsAsMap));
+    }
+
+    return;
   }
 
   @override
   Widget build(BuildContext context) {
     if (Platform.isAndroid) {
       return AndroidView(
-        viewType: "document_scanner",
+        viewType: _methodChannelIdentifier,
         creationParamsCodec: const StandardMessageCodec(),
-        creationParams: getParams(),
+        creationParams: _getParams(),
       );
     } else if (Platform.isIOS) {
       print("platform ios");
       return UiKitView(
-        onPlatformViewCreated: (int numre) {
-          print("native platform view created : ios : " + numre.toString());
-        },
-        viewType: "document_scanner",
-        creationParams: getParams(),
+        viewType: _methodChannelIdentifier,
+        creationParams: _getParams(),
         creationParamsCodec: const StandardMessageCodec(),
       );
     } else {
@@ -90,25 +94,27 @@ class _DocState extends State<DocumentScanner> {
     }
   }
 
-  Map<String, dynamic> getParams() {
-    Map<String, dynamic> allParams = {
-      "documentAnimation": widget.documentAnimation,
-      "overlayColor": widget.overlayColor,
-      "detectionCountBeforeCapture": widget.detectionCountBeforeCapture,
-      "enableTorch": widget.enableTorch,
-      "manualOnly": widget.manualOnly,
-      "noGrayScale": widget.noGrayScale,
-      "brightness": widget.brightness,
-      "contrast": widget.contrast,
-      "saturation": widget.saturation,
-    };
-    Map<String, dynamic> nonNullParams = {};
-    allParams.forEach((key, value) {
-      if (value != null) {
-        nonNullParams.addAll({key: value});
-      }
-    });
+  Map<String, dynamic> _getParams() {
+    // Map<String, dynamic> allParams = {
+    //   "documentAnimation": widget.documentAnimation,
+    //   "overlayColor": widget.overlayColor,
+    //   "detectionCountBeforeCapture": widget.detectionCountBeforeCapture,
+    //   "enableTorch": widget.enableTorch,
+    //   "manualOnly": widget.manualOnly,
+    //   "noGrayScale": widget.noGrayScale,
+    //   "brightness": widget.brightness,
+    //   "contrast": widget.contrast,
+    //   "saturation": widget.saturation,
+    // };
 
-    return nonNullParams;
+    // Map<String, dynamic> nonNullParams = {};
+    // allParams.forEach((key, value) {
+    //   if (value != null) {
+    //     nonNullParams.addAll({key: value});
+    //   }
+    // });
+
+    // return nonNullParams;
+    return {};
   }
 }

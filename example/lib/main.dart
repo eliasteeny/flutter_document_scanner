24b349fbd079f541d2ca5f1c0f46c708079126b4
elias teeny <elias.teeny@gmail.com>
2021-01-27 19:18:45 +0200

    handle camera permission from the example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 import 'package:document_scanner/document_scanner.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,9 +14,11 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   File scannedDocument;
+  Future<PermissionStatus> cameraPermissionFuture;
 
   @override
   void initState() {
+    cameraPermissionFuture = Permission.camera.request();
     super.initState();
   }
 
@@ -23,49 +26,67 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Stack(
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Expanded(
-                  child: scannedDocument != null
-                      ? Image(
-                          image: FileImage(scannedDocument),
-                        )
-                      : DocumentScanner(
-                          onDocumentScanned: (ScannedImage scannedImage) {
-                            print("document : " + scannedImage.croppedImage);
+          appBar: AppBar(
+            title: const Text('Plugin example app'),
+          ),
+          body: FutureBuilder<PermissionStatus>(
+            future: cameraPermissionFuture,
+            builder: (BuildContext context,
+                AsyncSnapshot<PermissionStatus> snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.data.isGranted)
+                  return Stack(
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Expanded(
+                            child: scannedDocument != null
+                                ? Image(
+                                    image: FileImage(scannedDocument),
+                                  )
+                                : DocumentScanner(
+                                    onDocumentScanned:
+                                        (ScannedImage scannedImage) {
+                                      print("document : " +
+                                          scannedImage.croppedImage);
 
-                            setState(() {
-                              scannedDocument =
-                                  scannedImage.getScannedDocumentAsFile();
-                              // imageLocation = image;
-                            });
-                          },
-                        ),
-                ),
-              ],
-            ),
-            scannedDocument != null
-                ? Positioned(
-                    bottom: 20,
-                    left: 0,
-                    right: 0,
-                    child: RaisedButton(
-                        child: Text("retry"),
-                        onPressed: () {
-                          setState(() {
-                            scannedDocument = null;
-                          });
-                        }),
-                  )
-                : Container(),
-          ],
-        ),
-      ),
+                                      setState(() {
+                                        scannedDocument = scannedImage
+                                            .getScannedDocumentAsFile();
+                                        // imageLocation = image;
+                                      });
+                                    },
+                                  ),
+                          ),
+                        ],
+                      ),
+                      scannedDocument != null
+                          ? Positioned(
+                              bottom: 20,
+                              left: 0,
+                              right: 0,
+                              child: RaisedButton(
+                                  child: Text("retry"),
+                                  onPressed: () {
+                                    setState(() {
+                                      scannedDocument = null;
+                                    });
+                                  }),
+                            )
+                          : Container(),
+                    ],
+                  );
+                else
+                  return Center(
+                    child: Text("camera permission denied"),
+                  );
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          )),
     );
   }
 }
